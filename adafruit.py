caa23be32b20520a832891b2990e94e519a73e5c
trungdai01 <quoctrung7802vn@gmail.com>
2024-03-29 00:12:09 +0700

    change paho-mqtt

--- adafruit.py
+++ adafruit.py
@@ -1,39 +1,69 @@
 import sys
-from Adafruit_IO import MQTTClient
 
-MQTT_SERVER = "mqttserver.tk"
+import paho.mqtt.client as mqtt
+import random
+import time
+
+MQTT_SERVER = "io.adafruit.com"
 MQTT_PORT = 1883
-MQTT_USERNAME = "innovation"
-MQTT_PASSWORD = "Innovation_RgPQAZoA5N"
-MQTT_TOPICS = ["button1", "button2"]
+MQTT_USERNAME = "trungdai"
+MQTT_PASSWORD = "aio_gxmi67yBYGNa4kLrMKPlTN6p34vY"
+MQTT_TOPICS = ["fan", "light"]
 MQTT_TEMP_TOPIC = "temp"
 MQTT_HUMID_TOPIC = "humid"
-MQTT_LIGHT_TOPIC = "light"
+MQTT_LIGHT_TOPIC = "led"
 MQTT_FAN_TOPIC = "fan"
 
-class Adafruit_MQTT:
 
-    def connected(self, client):
-        print("Connected ...")
-        for feed in MQTT_TOPICS:
-            client.subscribe(feed)
+class Paho_MQTT:
 
-    def subscribe(self, client , userdata , mid , granted_qos):
-        print("Subscribed...")
+    def __init__(self, broker_user, broker_pass):
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.client.username_pw_set(broker_user, broker_pass)
+        # self.self.client.connect(MQTT_SERVER, int(MQTT_PORT), 60)
 
-    def disconnected(self, client):
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
+
+    def connect(self):
+        self.client.connect(MQTT_SERVER, int(MQTT_PORT), 60)
+
+    def subscribe(self, topic):
+        self.client.subscribe(topic)
+
+    def publish(self, topic, message):
+        self.client.publish(topic, message)
+
+    def on_connect(self, client, userdata, flags, rc, properties):
+        if rc.is_failure:
+            print(f"Failed to connect: {rc}. loop_forever() will retry connection")
+        else:
+            print("Connected successfully")
+
+    def on_subscribe(self, client, userdata, mid, rc_list, properties):
+        if rc_list[0].is_failure:
+            print(f"Broker rejected your subscription: {rc_list[0]}")
+        else:
+            print(f"Broker granted the following QoS: {rc_list[0].value}")
+
+    def on_disconnect(self, client):
         print("Disconnected...")
-        sys.exit (1)
+        sys.exit(1)
 
-    def message(self, client , feed_id , payload):
-        print("Received: " + payload)
+    def on_message(self, client, userdata, message):
+        print(
+            "Received message '"
+            + str(message.payload.decode("utf-8"))
+            + "' on topic '"
+            + message.topic
+            + "' with QoS "
+            + str(message.qos)
+        )
 
-    def __init__(self):
-        client = MQTTClient(MQTT_USERNAME , MQTT_PASSWORD)
-        client.on_connect = self.connected
-        client.on_disconnect = self.disconnected
-        client.on_message = self.message
-        client.on_subscribe = self.subscribe
-        client.connect()
-        client.loop_background()
+    def loop_start(self):
+        self.client.loop_start()
 
+    # def published(self, topic, message):
+    #     self.client.publish(topic, message)
--- main.py
+++ main.py
@@ -1,3 +1,60 @@
 from adafruit import *
 
-obj = Adafruit_MQTT()
+# import paho.mqtt.client as mqtt
+# obj = Adafruit_MQTT()
+
+
+def mqtt_connected(client, userdata, flags, rc, properties):
+    if rc.is_failure:
+        print(f"Failed to connect: {rc}. loop_forever() will retry connection")
+    else:
+        # we should always subscribe from on_connect callback to be sure
+        # our subscribed is persisted across reconnections.
+        print("Connected successfully")
+        client.subscribe(MQTT_USERNAME + "/feeds/" + MQTT_FAN_TOPIC)
+        client.subscribe(MQTT_USERNAME + "/feeds/" + MQTT_LIGHT_TOPIC)
+
+
+def mqtt_subscribed(client, userdata, mid, rc_list, properties):
+    if rc_list[0].is_failure:
+        print(f"Broker rejected you subscription: {rc_list[0]}")
+    else:
+        print(f"Broker granted the following QoS: {rc_list[0].value}")
+
+
+def mqtt_message(client, userdata, msg):
+    print(
+        "Received message '"
+        + str(msg.payload.decode("utf-8"))
+        + "' on topic '"
+        + msg.topic
+        + "' with QoS "
+        + str(msg.qos)
+    )
+
+
+# client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+# client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+# client.connect(MQTT_SERVER, int(MQTT_PORT), 60)
+
+# client.on_connect = mqtt_connected
+# client.on_subscribe = mqtt_subscribed
+# client.on_message = mqtt_message
+
+# client.loop_start()
+
+if __name__ == "__main__":
+    mqttClient = Paho_MQTT(MQTT_USERNAME, MQTT_PASSWORD)
+    mqttClient.connect()
+    mqttClient.subscribe(MQTT_USERNAME + "/feeds/" + MQTT_FAN_TOPIC)
+    mqttClient.subscribe(MQTT_USERNAME + "/feeds/" + MQTT_LIGHT_TOPIC)
+    mqttClient.loop_start()
+    while True:
+        temperature = round(random.uniform(23.0, 34.0), 2)
+        mqttClient.publish(MQTT_USERNAME + "/feeds/" + MQTT_TEMP_TOPIC, temperature)
+        print(f"Publish temperature: {temperature} degrees Celsius successfully!")
+
+        humid = round(random.uniform(0.0, 101.0), 2)
+        mqttClient.publish(MQTT_USERNAME + "/feeds/" + MQTT_HUMID_TOPIC, humid)
+        print(f"Publish temperature: {humid}% successfully!")
+        time.sleep(15)
